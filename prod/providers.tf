1aab539f27971be09a4aaa3ca5c4af9c779474e1
s1199469 <rene.kouffeld@windesheim.nl>
2025-05-31 19:30:35 +0200

    Add files via upload

---
+++ prod/providers.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_version = ">= 0.13"
+  required_providers {
+    esxi = {
+      source = "registry.terraform.io/josenk/esxi"
+    }
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "~> 4.27.0"      
+    }
+  }
+}
